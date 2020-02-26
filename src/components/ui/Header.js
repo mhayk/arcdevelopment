83bf2c7bbdc0c85a1a59a1727dda4bde06dba265
Mhayk Whandson <hi@mhayk.com>
2020-02-26 18:00:27 +0000

    Ready to create the Theme provider..
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/components/ui/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import PropTypes from 'prop-types';
+
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+
+export default function Header(props) {
+  return (
+    <ElevationScroll>
+    <AppBar position="fixed">
+      <Toolbar>
+        Arc Development
+      </Toolbar>
+    </AppBar>
+    </ElevationScroll>
+  );
+}
