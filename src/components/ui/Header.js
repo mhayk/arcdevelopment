80877d055764d1af035771f3e28248020d0eaa61
Mhayk Whandson <hi@mhayk.com>
2020-03-01 20:55:16 +0000

    The behavior to update the hightlight color in the active tab after the page has been refreshed has been solved.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import PropTypes from 'prop-types';
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em',
   },
   logo: {
-    height: '7em',
+    height: '8em',
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -47,6 +47,12 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '50px',
     marginRight: '25px',
     height: '45px',
+  },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
 }));
 
@@ -66,12 +72,28 @@ export default function Header(props) {
 
   const handleChange = (e, value) => setValue(value);
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    }
+  }, [value, setValue]);
+
   return (
     <>
       <ElevationScroll>
         <AppBar position="fixed">
           <Toolbar disableGutters>
-            <img alt="company logo" src={logo} className={classes.logo} />
+            <Button component={Link} disableRipple to="/" onClick={() => setValue(0)} className={classes.logoContainer}>
+              <img alt="company logo" src={logo} className={classes.logo} />
+            </Button>
             <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
               <Tab className={classes.tab} component={Link} to="/" label="Home" />
               <Tab className={classes.tab} component={Link} to="/services" label="Services" />
