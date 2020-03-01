7afedfd06c439c942eacfd977cb5da6fd7efbf57
Mhayk Whandson <hi@mhayk.com>
2020-03-01 11:12:23 +0000

    The initial theme structure has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Header from '../components/ui/Header';
+import { ThemeProvider } from '@material-ui/styles';
+
+import theme from './ui/Theme';
+
+import Header from './ui/Header';
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header />
       Hello!
-    </div>
+    </ThemeProvider>
   );
 }
 
