7dfa1579cdc1de3221a62bbeb3de979bf863e539
Mhayk Whandson <hi@mhayk.com>
2020-03-08 22:45:01 +0000

    The MenuITem has been refactored.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -86,6 +86,7 @@ export default function Header(props) {
   const [value, setValue] = useState(0);
   const [anchorEl, setAnchorEl] = useState(null);
   const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleChange = (e, value) => setValue(value);
 
@@ -98,6 +99,13 @@ export default function Header(props) {
     setAnchorEl(null);
     setOpen(false);
   };
+
+  const menuOptions = [
+    { name: 'Services', link: '/services' },
+    { name: 'Custom Software Development', link: '/customsoftware' },
+    { name: 'Mobile App Development', link: '/mobileapps' },
+    { name: 'Website Development', link: '/websites' },
+  ];
 
   useEffect(() => {
     if (window.location.pathname === '/' && value !== 0) {
@@ -183,38 +191,18 @@ export default function Header(props) {
               MenuListProps={{ onMouseLeave: handleClose }}
               elevation={0}
             >
-              <MenuItem
-                onClick={() => { handleClose(); setValue(1); }}
-                component={Link}
-                to="/services"
-                classes={{ root: classes.menuItem }}
-              >
-                Services
-              </MenuItem>
-              <MenuItem
-                onClick={() => { handleClose(); setValue(1); }}
-                component={Link}
-                to="/customsoftware"
-                classes={{ root: classes.menuItem }}
-              >
-                Custom Software Development
-              </MenuItem>
-              <MenuItem
-                onClick={() => { handleClose(); setValue(1); }}
-                component={Link}
-                to="/mobileapps"
-                classes={{ root: classes.menuItem }}
-              >
-                Mobile App Development
-              </MenuItem>
-              <MenuItem
-                onClick={() => { handleClose(); setValue(1); }}
-                component={Link}
-                to="/websites"
-                classes={{ root: classes.menuItem }}
-              >
-                Website Development
-              </MenuItem>
+              {
+                menuOptions.map((item) => (
+                  <MenuItem
+                    onClick={() => { handleClose(); setValue(1); }}
+                    component={Link}
+                    to={item.link}
+                    classes={{ root: classes.menuItem }}
+                  >
+                    {item.name}
+                  </MenuItem>
+                ))
+              }
             </Menu>
           </Toolbar>
         </AppBar>
